1f60e8164544e413e56473727b4c3b9548055d25
Sebastián Kravetz <sebastiankravetz@icloud.com>
2025-01-21 23:11:23 -0300

    chore(swagger): adding openapi swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("com.fasterxml.jackson.core:jackson-databind")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.3")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.projectlombok:lombok")
